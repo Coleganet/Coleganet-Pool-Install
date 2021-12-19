cd4c81a274a1d6cec135c61e237539e1686e64c1
Coleganet <38811523+Coleganet@users.noreply.github.com>
2021-12-19 23:30:06 +0100

    update memcached

--- Install.sh
+++ Install.sh
@@ -99,6 +99,7 @@ default         0;
     sudo aptitude -y install php7.2-fpm
     sudo aptitude -y install php7.2-opcache php7.2-fpm php7.2 php7.2-common php7.2-gd php7.2-mysql php7.2-imap php7.2-cli php7.2-cgi php-pear php-auth php7.2-mcrypt mcrypt imagemagick libruby php7.2-curl php7.2-intl php7.2-pspell php7.2-recode php7.2-sqlite3 php7.2-tidy php7.2-xmlrpc php7.2-xsl memcached php-memcache php-imagick php-gettext php7.2-zip php7.2-mbstring php7.2-dev php7.2-dev
     sudo apt-get -y install php-memcache
+    sudo apt-get -y install memcached
     sudo apt-get -y install libmcrypt-dev
     sudo phpenmod mcrypt
     sudo pecl channel-update pecl.php.net
